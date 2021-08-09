d0fe17978002efe076795b8f3e325b013d36fceb
Harry potter <76622792+rayandgauba@users.noreply.github.com>
2021-08-09 14:37:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+function Philippines(){
+    window.location = "Philippines.html";
+}
+
+function Vietnam(){
+    window.location = "Vietnam.html";
+}
+
+function Indonesia(){
+    window.location = "Indonesia.html";
+}
+
+function Malaysia(){
+    window.location = "Malaysia.html";
+}
+
+function Thailand(){
+    window.location = "Thailand.html";
+}
+
+function India(){
+    window.location = "India.html";
+}
+
+function southKorea(){
+    window.location = "South Korea.html";
+}
+
+function China(){
+    window.location = "China.html";
+}
+
+function Singapore(){
+    window.location = "Singapore.html";
+}
+
+function Japan(){
+    window.location = "Japan.html";
+}
+
+function logOut(){
+    window.location = "index.html";
+}
